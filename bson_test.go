a985fa1ce279f29875a9ee511e381be8fd5cf19f
Divjot Arora <divjot.arora@10gen.com>
2020-06-25 12:54:22 -0400

    Add Unmarshal benchmarks for nested documents

--- bson_test.go
+++ bson_test.go
@@ -25,7 +25,7 @@ func BenchmarkMarshal(b *testing.B) {
 	}
 	for _, tc := range testCases {
 		b.Run(tc.name, func(b *testing.B) {
-			doc := getDocOfSize(tc.docSize)
+			doc := buildDoc(tc.docSize)
 
 			b.ReportAllocs()
 			for i := 0; i < b.N; i++ {
@@ -53,7 +53,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 	}
 	for _, tc := range testCases {
 		b.Run(tc.name, func(b *testing.B) {
-			doc := getRawDocOfSize(tc.docSize)
+			doc := buildRawDoc(tc.docSize)
 
 			b.ReportAllocs()
 			for i := 0; i < b.N; i++ {
@@ -67,8 +67,37 @@ func BenchmarkUnmarshal(b *testing.B) {
 	}
 }
 
-func getDocOfSize(n int) bson.D {
-	doc := bson.D{}
+func BenchmarkUnmarshalNested(b *testing.B) {
+	testCases := []struct {
+		name  string
+		depth int
+	}{
+		// No "empty" test because a doc without any nesting is already covered by BenchmarkUnmarshal
+		{"small", 1},
+		{"size 10", 10},
+		{"size 50", 50},
+		{"size 100", 100},
+		{"size 500", 500},
+		{"size 1000", 1000},
+	}
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			doc := buildNestedRawDoc(tc.depth)
+
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				var unmarshalled bson.D
+				err := bson.Unmarshal(doc, &unmarshalled)
+				if err != nil {
+					b.Error(err)
+				}
+			}
+		})
+	}
+}
+
+func buildDoc(n int) bson.D {
+	var doc bson.D
 	for i := 0; i < n; i++ {
 		key, val := getStringElement(i)
 		doc = append(doc, bson.E{key, val})
@@ -76,7 +105,15 @@ func getDocOfSize(n int) bson.D {
 	return doc
 }
 
-func getRawDocOfSize(n int) bsoncore.Document {
+func buildNestedDoc(n int) bson.D {
+	var doc bson.D
+	for i := 0; i < n; i++ {
+		doc = bson.D{{"x", doc}}
+	}
+	return doc
+}
+
+func buildRawDoc(n int) bsoncore.Document {
 	idx, doc := bsoncore.AppendDocumentStart(nil)
 	for i := 0; i < n; i++ {
 		key, val := getStringElement(i)
@@ -84,6 +121,23 @@ func getRawDocOfSize(n int) bsoncore.Document {
 	}
 
 	doc, _ = bsoncore.AppendDocumentEnd(doc, idx)
+	return doc
+}
+
+func buildNestedRawDoc(n int) bsoncore.Document {
+	idx, doc := bsoncore.AppendDocumentStart(nil)
+	key, val := getStringElement(n)
+	doc = bsoncore.AppendStringElement(doc, key, val)
+	doc, _ = bsoncore.AppendDocumentEnd(doc, idx)
+
+	for i := 0; i < n; i++ {
+		newIdx, newDoc := bsoncore.AppendDocumentStart(nil)
+		newDoc = bsoncore.AppendDocumentElement(newDoc, "x", doc)
+		newDoc, _ = bsoncore.AppendDocumentEnd(newDoc, newIdx)
+
+		doc = newDoc
+	}
+
 	return doc
 }
 

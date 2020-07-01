5b026dba939b5200bec2589f63f5b5250668dcc3
Divjot Arora <divjot.arora@10gen.com>
2020-07-01 15:16:56 -0400

    Add build-flagged command request/response documents

--- bson_test.go
+++ bson_test.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 )
 
@@ -226,92 +224,3 @@ func buildNestedRawDoc(n int) bsoncore.Document {
 func getStringElement(idx int) (key, value string) {
 	return fmt.Sprintf("field%v", idx), "blabla"
 }
-
-var (
-	isMasterResponse = bson.D{
-		{"ismaster", true},
-		{"maxBsonObjectSize", 16777216},
-		{"maxMessageSizeBytes", 48000000},
-		{"maxWriteBatchSize", 100000},
-		{"localTime", time.Now()},
-		{"logicalSessionTimeoutMinutes", 30},
-		{"minWireVersion", 0},
-		{"maxWireVersion", 6},
-		{"readOnly", false},
-		{"hostsBsonD", []interface{}{
-			bson.E{"host", "blabla1"},
-			bson.E{"host", "blabla2"},
-			bson.E{"host", "blabla3"},
-		}},
-		{"hostsIf", []interface{}{
-			bson.D{{"host", "blabla1"}},
-			bson.D{{"host", "blabla2"}},
-			bson.D{{"host", "blabla3"}},
-		}},
-	}
-
-	findOneRequest = bson.D{
-		{"find", "bla"},
-		{"$db", "test"},
-		{"filter", bson.D{{"b", 1}}},
-		{"limit", float64(1)},
-		{"singleBatch", true},
-		{"lsid", bson.D{
-			{"id", primitive.Binary{
-				Subtype: uint8(4),
-				Data:    []byte("blalblalbalblablalabl"),
-			}},
-		}},
-		{"$clusterTime", bson.D{
-			{"clusterTime", primitive.Timestamp{
-				T: uint32(1593340459),
-				I: uint32(1),
-			}},
-			{"signature", bson.D{
-				{"hash", primitive.Binary{
-					Subtype: uint8(4),
-					Data:    []byte("blalblalbalblablalablibibibibibibibi"),
-				}},
-				{"keyId", int64(6843344346754842627)},
-			}},
-		}},
-	}
-
-	findOneResponse = bson.D{
-		{"cursor", bson.D{
-			{"id", int64(0)},
-			{"ns", "eliot1-bla.test"},
-			{"firstBatch", bson.A{
-				bson.D{
-					{"_id", primitive.NewObjectID()},
-					{"a", 1},
-				},
-			}},
-		}},
-		{"$db", "test"},
-		{"ok", 1},
-		{"lsid", bson.D{
-			{"id", primitive.Binary{
-				Subtype: uint8(4),
-				Data:    []byte("blalblalbalblablalabl"),
-			}},
-		}},
-		{"$clusterTime", bson.D{
-			{"clusterTime", primitive.Timestamp{
-				T: uint32(1593340459),
-				I: uint32(1),
-			}},
-			{"signature", bson.D{
-				{"hash", primitive.Binary{
-					Subtype: uint8(4),
-					Data:    []byte("blalblalbalblablalablibibibibibibibi"),
-				}},
-				{"keyId", int64(6843344346754842627)},
-			}},
-		}},
-		{"operationTime", primitive.Timestamp{
-			T: uint32(1593340459),
-			I: uint32(1),
-		}},
-	}
-)
---
+++ command_constants_mgo.go
@@ -0,0 +1,89 @@
+// +build mgo
+
+package main
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	isMasterResponse = bson.D{
+		{"ismaster", true},
+		{"maxBsonObjectSize", 16777216},
+		{"maxMessageSizeBytes", 48000000},
+		{"maxWriteBatchSize", 100000},
+		{"localTime", time.Now()},
+		{"logicalSessionTimeoutMinutes", 30},
+		{"minWireVersion", 0},
+		{"maxWireVersion", 6},
+		{"readOnly", false},
+		{"hostsBsonD", []bson.D{
+			bson.D{{"host", "blabla1"}},
+			bson.D{{"host", "blabla2"}},
+			bson.D{{"host", "blabla3"}},
+		}},
+		{"hostsIf", []interface{}{
+			bson.D{{"host", "blabla1"}},
+			bson.D{{"host", "blabla2"}},
+			bson.D{{"host", "blabla3"}},
+		}},
+	}
+
+	findOneRequest = bson.D{
+		{"find", "bla"},
+		{"$db", "test"},
+		{"filter", bson.D{{"b", 1}}},
+		{"limit", float64(1)},
+		{"singleBatch", true},
+		{"lsid", bson.D{
+			{"id", bson.Binary{
+				Kind: uint8(4),
+				Data: []byte("blalblalbalblablalabl"),
+			}},
+		}},
+		{"$clusterTime", bson.D{
+			{"clusterTime", bson.MongoTimestamp(1593340459)},
+			{"signature", bson.D{
+				{"hash", bson.Binary{
+					Kind: uint8(4),
+					Data: []byte("blalblalbalblablalablibibibibibibibi"),
+				}},
+				{"keyId", int64(6843344346754842627)},
+			}},
+		}},
+	}
+
+	findOneResponse = bson.D{
+		{"cursor", bson.D{
+			{"id", int64(0)},
+			{"ns", "eliot1-bla.test"},
+			{"firstBatch", []bson.D{
+				bson.D{
+					{"_id", bson.NewObjectId()},
+					{"a", 1},
+				},
+			}},
+		}},
+		{"$db", "test"},
+		{"ok", 1},
+		{"lsid", bson.D{
+			{"id", bson.Binary{
+				Kind: uint8(4),
+				Data: []byte("blalblalbalblablalabl"),
+			}},
+		}},
+		{"$clusterTime", bson.D{
+			{"clusterTime", bson.MongoTimestamp(1593340459)},
+			{"signature", bson.D{
+				{"hash", bson.Binary{
+					Kind: uint8(4),
+					Data: []byte("blalblalbalblablalablibibibibibibibi"),
+				}},
+				{"keyId", int64(6843344346754842627)},
+			}},
+		}},
+		{"operationTime", bson.MongoTimestamp(1593340459)},
+	}
+)
